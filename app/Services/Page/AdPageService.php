0da3b3bd9843cc0d7c78a7e45921522c9ba5d954
LinYouXin <310467466@qq.com>
2021-07-14 10:18:05 +0800

    -

--- app/Services/Page/AdPageService.php
+++ app/Services/Page/AdPageService.php
@@ -68,7 +68,7 @@ class AdPageService extends PageService
             $adPage = (new AdPageModel())->where('id',$id)->first();
 
             $this->saveHtmlFile($adPage->n8_page_id,$data['html']);
-            $this->createReviewImg($page->n8_page_id,$data['review_img']);
+            $this->createReviewImg($adPage->n8_page_id,$data['review_img']);
 
             $this->createReviewImg($adPage->n8_page_id,$this->getHtmlFile($adPage->n8_page_id));
 
